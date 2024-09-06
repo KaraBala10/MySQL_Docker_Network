1e0b433792817581d80b518d4083f9de570a0e9a
KaraBala10 <mohammad.karabala@gmail.com>
2024-09-06 21:03:59 +0300

    test

--- init.sql
+++ init.sql
@@ -7,8 +7,5 @@ CREATE USER 'youruser'@'%' IDENTIFIED BY 'yourpassword';
 -- Grant privileges to the new user
 GRANT ALL PRIVILEGES ON production_db.* TO 'youruser'@'%' WITH GRANT OPTION;
 
--- Set the root password
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpassword';
-
 -- Apply changes
 FLUSH PRIVILEGES;
